5e9d2a1a9423215fcbf58a71c03df48acbdbc465
fy <qdwds94@gmail.com>
2021-01-05 11:39:24 +0800

    文件上传

--- 文件上传/app.js
+++ 文件上传/app.js
@@ -1,36 +1,22 @@
 const express = require('express');
 const app = express();
-const path = require('path');
-const multiparty = require('multiparty');
 const fs = require('fs');
+const multer = require('multer');
+const path = require('path');
+const DIR_PATH = path.join(__dirname, 'upload');
+const upload = multer({ dest: DIR_PATH }) //  文件存放的地方
 
 app.use(express.static(path.join(__dirname, 'public')))
-
-app.post('/file', (req, res) => {
-    const form = new multiparty.Form({
-        uploadDir: 'files'
-    })
-    form.parse(req, (err, fieds, files) => {
-
-        if (!err) {
-            const fileName = files.file[0].originalFilename;
-            const oldPath = __dirname + '\\' + files.file[0].path;
-            const newPath = __dirname + '\\' + 'files\\' + fileName
-            console.log(oldPath);
-            fs.rename(oldPath, newPath, (err, q) => {
-                if (!err) {
-                    res.send(
-                        {
-                            message: "上传成功",
-                            code: 200
-                        }
-                    )
-                }
-            })
-
-        }
-
-    })
-
+app.get('/', (req, res) => {
+    res.send('123')
 })
-app.listen(3000, () => console.log(`http://localhost:3000`))
+
+app.post('/file', upload.single('file'), (req, res) => {
+    const oldFilePath = req.file.path;
+    const newFilePath = path.join(DIR_PATH, req.file.originalname);
+    fs.rename(oldFilePath, newFilePath, (err) => {
+        if (err) return console.log(err);
+        res.send('ok')
+    })
+})
+app.listen(3000, () => console.log(3000))
