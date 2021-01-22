645ee918a92f2add7f8c1685a5798d04539144b9
fy <qdwds94@gmail.com>
2021-01-22 10:35:29 +0800

    文件夹上传

--- 文件夹上传/app.js
+++ 文件夹上传/app.js
@@ -3,28 +3,38 @@ const app = express();
 const path = require('path')
 const multer = require('multer');
 const fs = require('fs');
-const dir_path = path.join(__dirname,'upload');
-const upload_path = multer({dest:dir_path});
-app.use(express.static(path.join(__dirname,'public')));
+const dir_path = path.join(__dirname, 'upload');
+const upload_path = multer({ dest: dir_path });
+app.use(express.static(path.join(__dirname, 'public')));
 
-app.post('/files',upload_path.array('file'),(req,res)=>{
-    console.log(req.files);
-    console.log(req.body);
+app.post('/files', upload_path.array('file'), (req, res) => {
+    // console.log(req.files);
+    // console.log(req.body);
     //  剔除文件名
     const filePathList = req.body.path.split('/');
     filePathList.pop()
-    createDir(filePathList)
-    res.send('ok')
+    let filePath = filePathList.join('/')
+    let createPaths = path.join(dir_path, filePath)
+
+    let files =[];
+
+    fs.mkdir(createPaths, { recursive: true }, (err) => {
+        if (err) throw err;
+
+        //  把下载的文件放到指定目录下
+        let oldPath = req.files[0].path;
+        let newPath = path.join(dir_path, req.body.path)
+        fs.rename(oldPath, newPath, (err) => {
+            if (err) throw err;
+            files.push(newPath)
+            res.send({
+                success:2000,
+                path:newPath
+            })
+
+        })
+    });
 })
 
-function createDir(arr){
-   for (let i = 0; i < arr.length; i++) {
-         fs.mkdir(path.join(dir_path,arr[i]),(err)=>{
-             console.log(err);
-             arr.shift();
-         })
-   }
-}
 
-
-app.listen(3000,()=>console.log(3000))
+app.listen(3000, () => console.log(3000))
