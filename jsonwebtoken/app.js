c54c22d557911e2c5787d9a2a45b0774c2744d7d
qdwds <52602678+qdwds@users.noreply.github.com>
2021-02-17 17:23:56 +0800

    Update app.js

--- jsonwebtoken/app.js
+++ jsonwebtoken/app.js
@@ -5,6 +5,8 @@ const jwt = require('jsonwebtoken');
 app.use(express.static(path.join(__dirname,'public')))
 
 app.get('/jwt',(req,res)=>{
+    //  参数一  应该是用户的唯一id
+    //  参数二  应该放到全局变量里面 ，唯一的标识符
     const token = jwt.sign({data:'123'},'secrent1',{expiresIn:'20s'})
     res.send({
         code:200,
@@ -14,6 +16,7 @@ app.get('/jwt',(req,res)=>{
 
 app.get('/check',(req,res)=>{
     const token = req.headers['authorization']
+      //  参数二  应该放到全局变量里面 ，唯一的标识符
     jwt.verify(token, 'secrent1',function(err, decoded) {
         if (err)  return res.send({
             ...err,
